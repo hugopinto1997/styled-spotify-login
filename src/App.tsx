b3c89db8c1e4d9419d4b23865ddc436fa411e437
hugopinto1997 <hugopillo@hotmail.com>
2022-10-12 10:34:23 -0600

    ♻️ feat: update project App component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import BasicLayout from 'layout/BasicLayout';
+import Login from 'views/Login';
+
+function App() {
+  return (
+    <BasicLayout>
+      <Login />
+    </BasicLayout>
+  );
+}
+
+export default App;
